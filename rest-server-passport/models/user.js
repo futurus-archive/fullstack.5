a21927ef1ed7429c9b5e50efa2216bded2207c49
Vu Nguyen <vnnguye2@illinois.edu>
2017-01-16 00:28:41 -0500

    mongoose population exercise completed.

--- rest-server-passport/models/user.js
+++ rest-server-passport/models/user.js
@@ -8,11 +8,23 @@ var passportLocalMongoose = require('passport-local-mongoose');
 var User = new Schema({
     username: String,
     password: String,
-    admin: {
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
+    admin:   {
         type: Boolean,
         default: false
     }
 });
+
+User.methods.getName = function() {
+    return (this.firstname + ' ' + this.lastname);
+};
 
 User.plugin(passportLocalMongoose);
 
